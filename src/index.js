821542493e40e00f3b5821f036cd44be04c8b8fa
Alasdair McLeay <alasdair.mcleay@me.com>
2018-04-27 19:47:22 +0100

    react router initial poc

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React, {Component} from 'react'
+import { Switch, Route } from 'react-router';
 
 export default class extends Component {
   render() {
-    return <div>
-      <h2>Welcome to React components</h2>
-    </div>
+    return <Switch>
+      <Route exact path="/" render={props => <div><h1>Home</h1><pre>{JSON.stringify{props}}}</pre></div>} />
+      <Route path="/about" render={props => <div><h1>About</h1><pre>{JSON.stringify{props}}}</pre></div>} />
+      <Route path="/:user" render={props => <div><h1>User</h1><pre>{JSON.stringify{props}}}</pre></div>} />
+      <Route render={props => <div><h1>404</h1><pre>{JSON.stringify{props}}}</pre></div>} />
+    </Switch>
   }
 }
