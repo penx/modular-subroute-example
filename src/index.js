7635c8390476d670d2e0c94098b28dba7b2a16dd
Alasdair McLeay <alasdair.mcleay@me.com>
2018-04-27 20:43:28 +0100

    user id in title

--- src/index.js
+++ src/index.js
@@ -14,16 +14,16 @@ export default class extends Component {
             <Link to="/about">About</Link>
           </li>
           <li>
-            <Link to="/user1">User 1</Link>
+            <Link to="/1">User 1</Link>
           </li>
           <li>
-            <Link to="/user2">User 2</Link>
+            <Link to="/2">User 2</Link>
           </li>
         </ul>
       <Switch>
         <Route exact path="/" render={props => <div><h1>Home</h1><pre>{JSON.stringify(props)}}</pre></div>} />
         <Route path="/about" render={props => <div><h1>About</h1><pre>{JSON.stringify(props)}}</pre></div>} />
-        <Route path="/:user" render={props => <div><h1>User</h1><pre>{JSON.stringify(props)}}</pre></div>} />
+        <Route path="/:user" render={props => <div><h1>User {props.match.params.user}</h1><pre>{JSON.stringify(props)}}</pre></div>} />
         <Route render={props => <div><h1>404</h1><pre>{JSON.stringify(props)}}</pre></div>} />
       </Switch>
     </div>
