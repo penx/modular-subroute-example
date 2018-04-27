c4b80d8026baefbd5e3f73d4a66b1f9f88569d43
Alasdair McLeay <alasdair.mcleay@me.com>
2018-04-27 19:50:40 +0100

    correct brackets

--- src/index.js
+++ src/index.js
@@ -4,10 +4,10 @@ import { Switch, Route } from 'react-router';
 export default class extends Component {
   render() {
     return <Switch>
-      <Route exact path="/" render={props => <div><h1>Home</h1><pre>{JSON.stringify{props}}}</pre></div>} />
-      <Route path="/about" render={props => <div><h1>About</h1><pre>{JSON.stringify{props}}}</pre></div>} />
-      <Route path="/:user" render={props => <div><h1>User</h1><pre>{JSON.stringify{props}}}</pre></div>} />
-      <Route render={props => <div><h1>404</h1><pre>{JSON.stringify{props}}}</pre></div>} />
+      <Route exact path="/" render={props => <div><h1>Home</h1><pre>{JSON.stringify(props)}}</pre></div>} />
+      <Route path="/about" render={props => <div><h1>About</h1><pre>{JSON.stringify(props)}}</pre></div>} />
+      <Route path="/:user" render={props => <div><h1>User</h1><pre>{JSON.stringify(props)}}</pre></div>} />
+      <Route render={props => <div><h1>404</h1><pre>{JSON.stringify(props)}}</pre></div>} />
     </Switch>
   }
 }
