94845ac4995829ff5f32bd205a27a8eae3ee31e1
Alasdair McLeay <alasdair.mcleay@me.com>
2018-04-27 20:32:22 +0100

    add react router dom dependency

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react'
 import { Switch, Route } from 'react-router';
+import { Link } from 'react-router-dom';
 
 export default class extends Component {
   render() {
